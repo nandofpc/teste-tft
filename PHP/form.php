021db897432c8831d55285851e0a6d4898bfe0db
m307219 <fernando.p.coelho@bradesco.com.br>
2022-11-13 10:23:13 -0300

    alteração

--- PHP/form.php
+++ PHP/form.php
@@ -2,8 +2,9 @@
 <body>
 
 <form action="welcome_get.php" method="get">
-<p>Name: <input type="text" name="name"></p>
-<p>E-mail: <input type="text" name="email"></p>
+<label>IP Address</label>
+<p><input type="text" class="form-input" id="ip" name="ip" placeholder="xxx.xxx.xxx.xxx"/></p>
+<p>Hostname: <input type="text" name="hostname"></p>
 <input type="submit">
 </form>
 
--- PHP/welcome_get.php
+++ PHP/welcome_get.php
@@ -1,8 +1,13 @@
-<html>
-<body>
-Welcome <?php echo $_GET["name"]; ?>
-<br>
-Your email address is: <?php echo $_GET["email"]; ?>
+<?php 
 
-</body>
-</html>
+# Update DNS Record
+
+$AMBIENTE = $_GET["hostname"]; 
+$DOMAIN = "arodns.local";
+$IPLBAPI = $_GET["ip"];
+$COMANDO = 'Add-DnsServerResourceRecordA -Name '. $AMBIENTE; // &'-ZoneName $DOMAIN -IPv4Address $IPLBAPI';
+echo '<br>';
+echo $COMANDO;
+//$data = (shell_exec('nslookup ' . $ambiente));
+//echo $data;
+?>
