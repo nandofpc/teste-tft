f4b0adfa0c22bc9490e5ce740834ead1b06004d1
m307219 <fernando.p.coelho@bradesco.com.br>
2022-11-13 11:12:11 -0300

    alteração

--- PHP/welcome_get.php
+++ PHP/welcome_get.php
@@ -5,9 +5,9 @@
 $AMBIENTE = $_GET["hostname"]; 
 $DOMAIN = "arodns.local";
 $IPLBAPI = $_GET["ip"];
-$COMANDO = 'Add-DnsServerResourceRecordA -Name '. $AMBIENTE; // &'-ZoneName $DOMAIN -IPv4Address $IPLBAPI';
+$COMANDO = 'Add-DnsServerResourceRecordA -Name '. $AMBIENTE . ' -ZoneName '. $DOMAIN . ' -IPv4Address '. $IPLBAPI;
 echo '<br>';
-echo $COMANDO;
+$data = (shell_exec('powershell ' . $COMANDO));
 //$data = (shell_exec('nslookup ' . $ambiente));
-//echo $data;
+echo $data;
 ?>
